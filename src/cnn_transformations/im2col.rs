5dc732ec954d7f3f0fc9db5bd46c989413d4dfd6
gckohaku <cyanhairneko@gmail.com>
2025-11-17 16:12:56 +0900

    edit im2col

--- src/cnn_transformations/im2col.rs
+++ src/cnn_transformations/im2col.rs
@@ -1,6 +1,23 @@
 use ndarray::ArrayD;
 
-pub fn im2col(images: &ArrayD<f64>, filters: &ArrayD<f64>, stride: usize, padding: usize) {
-    
+pub fn im2col(inputs: &ArrayD<f64>, filters: &ArrayD<f64>, stride: usize, padding: usize) {
+    // パディングとストライドについては今は考えずに、入力テンソルの次元も 4 であるとする
 
+	if inputs.ndim() != 4 || filters.ndim() != 4 {
+		panic!("次元数が4でない");
+	}
+
+	// 出力データ (行列) のサイズを計算
+	// そのために、まずは入力データのそれぞれの次元のサイズを取得
+	let inputs_shape = inputs.shape();
+	let filters_shape = filters.shape();
+
+	let batch_value = inputs_shape[0];
+	let input_channel_value = inputs_shape[1];
+	let input_size = (inputs_shape[2], inputs_shape[3]);
+	let output_channel_value = filters_shape[0];
+	let filter_size = (filters_shape[2], filters_shape[3]);
+	let output_size = filter_size.0 * filter_size.1;
+
+	// TODO: 行列のサイズを計算
 }
