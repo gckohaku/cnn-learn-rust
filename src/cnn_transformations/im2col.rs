0bd526255154773dd00d8df683915020d955a92b
gckohaku <cyanhairneko@gmail.com>
2025-11-18 16:11:11 +0900

    im2col without stride and padding dot 演算を利用すると、リンクエラーが起きる (linalg が無いから？)

--- src/cnn_transformations/im2col.rs
+++ src/cnn_transformations/im2col.rs
@@ -1,23 +1,66 @@
-use ndarray::ArrayD;
+use std::fs;
 
-pub fn im2col(inputs: &ArrayD<f64>, filters: &ArrayD<f64>, stride: usize, padding: usize) {
+use ndarray::{Array2, Array4, Array6, ArrayD, Shape, s};
+
+pub fn im2col(inputs: &Array4<f64>, filters: &Array4<f64>, stride: usize, padding: usize) -> (Array2<f64>, Array2<f64>) {
     // パディングとストライドについては今は考えずに、入力テンソルの次元も 4 であるとする
 
-	if inputs.ndim() != 4 || filters.ndim() != 4 {
-		panic!("次元数が4でない");
-	}
+    if inputs.ndim() != 4 || filters.ndim() != 4 {
+        panic!("次元数が4でない");
+    }
 
-	// 出力データ (行列) のサイズを計算
-	// そのために、まずは入力データのそれぞれの次元のサイズを取得
-	let inputs_shape = inputs.shape();
-	let filters_shape = filters.shape();
+    // 出力データ (行列) のサイズを計算
+    // そのために、まずは入力データのそれぞれの次元のサイズを取得
+    let inputs_shape = inputs.shape();
+    let filters_shape = filters.shape();
 
-	let batch_value = inputs_shape[0];
-	let input_channel_value = inputs_shape[1];
-	let input_size = (inputs_shape[2], inputs_shape[3]);
-	let output_channel_value = filters_shape[0];
-	let filter_size = (filters_shape[2], filters_shape[3]);
-	let output_size = filter_size.0 * filter_size.1;
+    let batch_value = inputs_shape[0];
+    let input_channel_value = inputs_shape[1];
+    let input_size = (inputs_shape[2], inputs_shape[3]);
+    let output_channel_value = filters_shape[0];
+    let filter_size = (filters_shape[2], filters_shape[3]);
+    let output_size = (
+        input_size.0 - filter_size.0 + 1,
+        input_size.1 - filter_size.1 + 1,
+    );
 
-	// TODO: 行列のサイズを計算
+    // 行列のサイズを計算
+    let output_matrix_size = (
+        output_channel_value,
+        batch_value * output_size.0 * output_size.1,
+    );
+
+    let mut processing_tensor: Array6<f64> = Array6::zeros((
+        batch_value,
+        input_channel_value,
+        filter_size.0,
+        filter_size.1,
+        output_size.0,
+        output_size.1,
+    ));
+
+    println!("tensor: {:?}", processing_tensor.shape());
+
+    for h in 0..filter_size.0 {
+        for w in 0..filter_size.1 {
+            processing_tensor
+                .slice_mut(s![.., .., h, w, .., ..])
+                .assign(&inputs.slice(s![.., .., h..(h + output_size.0), w..(w + output_size.1)]));
+        }
+    }
+
+    processing_tensor.swap_axes(0, 1);
+    processing_tensor.swap_axes(1, 2);
+    processing_tensor.swap_axes(2, 3);
+
+    let input_col_matrix = processing_tensor
+        .to_shape((
+            input_channel_value * filter_size.0 * filter_size.1,
+            batch_value * output_size.0 * output_size.1,
+        ))
+        .unwrap();
+
+	let filter_col_matrix = filters.to_shape((output_channel_value, input_channel_value * filter_size.0 * filter_size.1)).unwrap();
+
+	(input_col_matrix.to_owned(), filter_col_matrix.to_owned())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+use ndarray::{Array, Array2, Array4};
+
+use crate::cnn_transformations::im2col::{im2col};
+
 mod cnn_transformations;
 
 fn main() {
-    println!("Hello, world!");
+    let x: Array4<f64> = Array::from_vec((1..(3 * 3 * 4 * 4 + 1)).map(|x| x as f64).collect())
+        .to_shape((3, 3, 4, 4))
+        .unwrap()
+        .to_owned();
+    let f: Array4<f64>=
+        Array::from_vec((-(3 * 3 * 2 * 2)..0).map(|x| x as f64).collect())
+            .to_shape((3, 3, 2, 2))
+            .unwrap()
+            .to_owned();
+
+    println!("{:?}\n", im2col(&x, &f, 0, 0));
+
+    let (a, b) = im2col(&x, &f, 0, 0);
+
+    println!("{:?}", a.dot(&b));
 }
