542b1b870c3c4394dfe5b446b798c4294bd9a0e1
gckohaku <cyanhairneko@gmail.com>
2025-11-21 16:08:12 +0900

    wip

--- src/cnn_transformations/im2col.rs
+++ src/cnn_transformations/im2col.rs
@@ -1,12 +1,24 @@
 use std::fs;
 
-use ndarray::{Array2, Array4, Array6, ArrayD, Shape, s};
+use ndarray::{Array2, Array4, Array6, ArrayD, Shape, Slice, s};
 
-pub fn im2col(inputs: &Array4<f64>, filters: &Array4<f64>, stride: usize, padding: usize) -> (Array2<f64>, Array2<f64>) {
-    // パディングとストライドについては今は考えずに、入力テンソルの次元も 4 であるとする
+pub fn im2col(
+    inputs: &Array4<f64>,
+    filters: &Array4<f64>,
+    stride: usize,
+    padding: usize,
+) -> (Array2<f64>, Array2<f64>) {
+    if stride <= 0 {
+        println!("{}, {}", stride, stride <= 0);
+        panic!("'stride' must be greater than zero.");
+    }
 
     if inputs.ndim() != 4 || filters.ndim() != 4 {
         panic!("次元数が4でない");
+    }
+
+    if padding > 0 {
+        inputs
     }
 
     // 出力データ (行列) のサイズを計算
@@ -20,8 +32,8 @@ pub fn im2col(inputs: &Array4<f64>, filters: &Array4<f64>, stride: usize, paddin
     let output_channel_value = filters_shape[0];
     let filter_size = (filters_shape[2], filters_shape[3]);
     let output_size = (
-        input_size.0 - filter_size.0 + 1,
-        input_size.1 - filter_size.1 + 1,
+        (input_size.0 - filter_size.0) / stride + 1,
+        (input_size.1 - filter_size.1) / stride + 1,
     );
 
     // 行列のサイズを計算
@@ -43,7 +55,12 @@ pub fn im2col(inputs: &Array4<f64>, filters: &Array4<f64>, stride: usize, paddin
         for w in 0..filter_size.1 {
             processing_tensor
                 .slice_mut(s![.., .., h, w, .., ..])
-                .assign(&inputs.slice(s![.., .., h..(h + output_size.0), w..(w + output_size.1)]));
+                .assign(&inputs.slice(s![
+                    ..,
+                    ..,
+                    Slice::from(h..(h + output_size.0 * stride).min(inputs_shape[2])).step_by(stride.try_into().unwrap()),
+                    Slice::from(w..(w + output_size.1 * stride).min(inputs_shape[3])).step_by(stride.try_into().unwrap())
+                ]));
         }
     }
 
@@ -56,7 +73,12 @@ pub fn im2col(inputs: &Array4<f64>, filters: &Array4<f64>, stride: usize, paddin
         ))
         .unwrap();
 
-	let filter_col_matrix = filters.to_shape((output_channel_value, input_channel_value * filter_size.0 * filter_size.1)).unwrap();
+    let filter_col_matrix = filters
+        .to_shape((
+            output_channel_value,
+            input_channel_value * filter_size.0 * filter_size.1,
+        ))
+        .unwrap();
 
-	(input_col_matrix.to_owned(), filter_col_matrix.to_owned())
+    (input_col_matrix.to_owned(), filter_col_matrix.to_owned())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use ndarray::{Array, Array2, Array4};
 
-use crate::cnn_transformations::im2col::{im2col};
+use crate::cnn_transformations::im2col::im2col;
 
 mod cnn_transformations;
 
@@ -9,15 +9,15 @@ fn main() {
         .to_shape((3, 3, 4, 4))
         .unwrap()
         .to_owned();
-    let f: Array4<f64>=
-        Array::from_vec((-(3 * 3 * 2 * 2)..0).map(|x| x as f64).collect())
-            .to_shape((3, 3, 2, 2))
-            .unwrap()
-            .to_owned();
+    let f: Array4<f64> = Array::from_vec((-(3 * 3 * 2 * 2)..0).map(|x| x as f64).collect())
+        .to_shape((3, 3, 2, 2))
+        .unwrap()
+        .to_owned();
 
-    println!("{:?}\n", im2col(&x, &f, 0, 0));
+    let stride = 2;
+    let padding = 0;
 
-    let (a, b) = im2col(&x, &f, 0, 0);
-
+    let (a, b) = im2col(&x, &f, stride, padding);
+    println!("{:?}\n", (&a, &b));
     println!("{:?}", b.dot(&a));
 }
