b4b9bfb7f1dcd87a0303005f93335c6b9dad21db
gckohaku <cyanhairneko@gmail.com>
2025-12-02 14:24:22 +0900

    edit cnn_network.rs

--- src/convolution_network.rs
+++ src/convolution_network.rs
@@ -1,11 +1,15 @@
-use ndarray::{Array1, Array4, Dimension};
+use ndarray::{Array1, Array2, Array4, Dimension};
 
-use crate::cnn_information::{ConvolutionInformation, LayerInformation, LayerType, OutputType, PoolingInformation};
+use crate::cnn_information::{
+    ConvolutionInformation, LayerInformation, LayerType, OutputType, PoolingInformation,
+};
 
 pub struct ConvolutionNetwork {
     pub layers_information: Vec<LayerInformation>,
     pub filters: Vec<Array4<f64>>,
     pub biases: Vec<Array1<f64>>,
+    pub windows: Vec<Array2<f64>>,
+    pub switches: Vec<Array2<(usize, usize)>>,
     pub values: Vec<Array4<f64>>,
     pub values_after_activation: Vec<Array4<f64>>,
 }
@@ -19,50 +23,64 @@ impl ConvolutionNetwork {
     ) -> Self {
         let mut filters = Vec::<Array4<f64>>::new();
         let mut biases = Vec::<Array1<f64>>::new();
+        let mut windows = Vec::<Array2<f64>>::new();
+        let mut switches = Vec::<Array2<(usize, usize)>>::new();
         let mut values = Vec::<Array4<f64>>::new();
         let mut values_after_activation = Vec::<Array4<f64>>::new();
 
-		let mut before_image_shape = [batch_size, input_channel_value, input_image_size.0, input_image_size.1];
+        let mut before_image_shape = [
+            batch_size,
+            input_channel_value,
+            input_image_size.0,
+            input_image_size.1,
+        ];
 
         for i in 0..layers_information.len() {
+            let input_size = (before_image_shape[2], before_image_shape[3]);
+
             if layers_information[i].layer_type == LayerType::Convolution {
-                let image_shape = values[i - 1].shape();
                 let info: &ConvolutionInformation =
                     layers_information[i].information.as_convolution().unwrap();
-                let output_image_height =
-                    (before_image_shape[2] - info.filter_size.0 + 2 * info.padding) / info.stride + 1;
-                let output_image_width =
-                    (before_image_shape[3] - info.filter_size.1 + 2 * info.padding) / info.stride + 1;
 
                 let (weight, bias, value) = Self::create_convolution_layer(
                     batch_size,
-                    input_channel_value,
-                    input_image_size,
+                    before_image_shape[1],
+                    input_size,
                     info,
                 );
 
-				before_image_shape = value.shape().try_into().unwrap();
+                filters.push(weight);
+                biases.push(bias);
+                values.push(value.clone());
+                values_after_activation.push(value);
+            } else if layers_information[i].layer_type == LayerType::Pooling {
+                let info: &PoolingInformation =
+                    layers_information[i].information.as_pooling().unwrap();
 
-				filters.push(weight);
-				biases.push(bias);
-				values.push(value.clone());
-				values_after_activation.push(value);
+                let (window, switch, output) =
+                    Self::create_pooling_layer(batch_size, before_image_shape[1], input_size, info);
+
+                windows.push(window);
+                switches.push(switch);
+                values.push(output.clone());
+                values_after_activation.push(output);
             }
-			else if layers_information[i].layer_type == LayerType::Pooling {
 
-			}
+            before_image_shape = values.last().unwrap().shape().try_into().unwrap();
         }
 
         Self {
             layers_information,
             filters,
             biases,
+            windows,
+            switches,
             values,
             values_after_activation,
         }
     }
 
-	fn create_convolution_layer(
+    fn create_convolution_layer(
         batch_size: usize,
         input_channel_value: usize,
         input_image_size: (usize, usize),
@@ -103,7 +121,7 @@ impl ConvolutionNetwork {
         input_channel_value: usize,
         input_image_size: (usize, usize),
         info: &PoolingInformation,
-    ) -> (Array4<f64>, Array4<f64>) {
+    ) -> (Array2<f64>, Array2<(usize, usize)>, Array4<f64>) {
         let output_channel_value = input_channel_value;
         let window_size = info.window_size;
         let output_image_size = (
@@ -111,8 +129,17 @@ impl ConvolutionNetwork {
             input_image_size.1 / window_size.1,
         );
 
-        let output = Array4::zeros([batch_size, output_channel_value, output_image_size.0, output_image_size.1]);
+        let window = Array2::zeros([window_size.0, window_size.1]);
 
-        (output.clone(), output)
+        let switch = Array2::from_elem([window_size.0, window_size.1], (0, 0));
+
+        let output = Array4::zeros([
+            batch_size,
+            output_channel_value,
+            output_image_size.0,
+            output_image_size.1,
+        ]);
+
+        (window, switch, output)
     }
 }
