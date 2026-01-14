88f69bb62365a2326dd5b121f5a3dccab0c20c2a
gckohaku <cyanhairneko@gmail.com>
2026-01-14 16:08:05 +0900

    WIP #27

--- src/nn_modules.rs
+++ src/nn_modules.rs
@@ -4,12 +4,14 @@ pub mod relu;
 pub mod softmax;
 pub mod cross_entropy_loss;
 pub mod softmax_and_celoss;
+pub mod nn_directed_graph;
 
 pub use linear::Linear;
 pub use relu::ReLU;
 pub use softmax::Softmax;
 pub use cross_entropy_loss::CrossEntropyLoss;
 pub use softmax_and_celoss::SoftmaxAndCELoss;
+pub use nn_directed_graph::NNDirectedGraph;
 
 pub trait NNModule {
 	type InputArray<'a>;
