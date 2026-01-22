07f3618f5686b143eb7b456fa2562f6dc1103e35
gckohaku <cyanhairneko@gmail.com>
2026-01-22 16:11:36 +0900

    WIP #27

--- src/nn_modules/nn_directed_graph.rs
+++ src/nn_modules/nn_directed_graph.rs
@@ -1,3 +1,19 @@
-pub struct NNDirectedGraph {
-	
+use ndarray::ArrayD;
+
+use crate::nn_modules::{NNForwardInput, NNGraphNode, NNModule};
+
+pub struct NNDirectedGraph<'a, T> {
+	pub graph: NNGraphNode<'a, T>
+}
+
+impl<'a, T> NNModule<T> for NNDirectedGraph<'a, T> {
+	fn forward(&mut self, input: &NNForwardInput<'_, '_, T>) -> ArrayD<T> {
+		let parent_data = self.graph.module_data.forward(input);
+	}
+}
+
+impl<'a, T> NNDirectedGraph<'a, T> {
+	pub fn recursion_forward(&mut self, input: &NNForwardInput<'_, '_, T>, node: &NNGraphNode<'a, T>) -> ArrayD<T> {
+		let parent_data = self.graph.module_data.forward(input);
+	}
 }
--- src/nn_modules/nn_graph_node.rs
+++ src/nn_modules/nn_graph_node.rs
@@ -1,7 +1,7 @@
 use crate::nn_modules::NNModule;
 
 // ループごとに新しくインスタンスを生成するか、Vec<Arc<T>> にすることを検討した方がいいかも
-pub struct NNGraphNode<'a, 'b, T> {
+pub struct NNGraphNode<'a, T> {
 	pub module_data: &'a dyn NNModule<T>,
-	pub child_nodes: Vec<&'b dyn NNModule<T>>,
+	pub child_nodes: Vec<NNGraphNode<'a, T>>,
 }
