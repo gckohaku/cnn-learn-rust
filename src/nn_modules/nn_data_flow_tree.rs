1def8a991c4b762aaac38ffe55f86bd98981dd50
gckohaku <cyanhairneko@gmail.com>
2026-01-27 16:09:38 +0900

    WIP #28

--- src/nn_modules/nn_data_flow_tree.rs
+++ src/nn_modules/nn_data_flow_tree.rs
@@ -1,17 +1,49 @@
-use crate::nn_modules::{NNDataStreamNode, NNModule};
+use ndarray::{
+    ArrayD, IxDyn,
+};
+use num_traits::Zero;
+
+use crate::nn_modules::{NNDataStreamNode, NNForwardInput, NNModule};
 
 pub struct NNDataFlowTree<'a, T> {
-	pub tree: Vec<NNDataStreamNode<'a, T>>,
+    pub tree: Vec<NNDataStreamNode<'a, T>>,
+	result: ArrayD<T>,
 }
 
-// impl<'a, T> NNModule<T> for NNDataFlowTree<'a, T> {
+struct TopologicalSortedList<'a, T> {
 	
-// }
+}
+
+impl<'a, T> NNModule<T> for NNDataFlowTree<'a, T>
+where
+    T: Clone + Zero,
+{
+    fn forward(&mut self, input: &NNForwardInput<'_, '_, T>) -> ArrayD<T> {
+        for node in &self.tree {}
+
+        ArrayD::<T>::zeros(IxDyn(&vec![0]))
+    }
+}
+
+fn recursive_forward<'a, T>(stream_node: &mut NNDataStreamNode<'a, T>, input: &NNForwardInput<'_, '_, T>)
+where
+    T: Clone + Zero,
+{
+	let data = stream_node.module_data.forward(input);
+
+	if stream_node.children.len() == 0 {
+		
+	}
+
+	for node in &stream_node.children {
+		_ = recursive_forward(stream_node, input);
+	}
+}
 
 impl<'a, T> NNDataFlowTree<'a, T> {
-	pub fn new() -> Self {
-		NNDataFlowTree::<T> {
-			tree: Vec::<NNDataStreamNode<'_, T>>::new(),
-		}
-	}
+    pub fn new() -> Self {
+        NNDataFlowTree::<T> {
+            tree: Vec::<NNDataStreamNode<'_, T>>::new(),
+        }
+    }
 }
