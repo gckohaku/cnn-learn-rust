1d088c7415be34ec122c9e59f797ad5f78e939a5
gckohaku <cyanhairneko@gmail.com>
2026-05-13 15:57:31 +0900

    WIP #52 20260513-01

--- src/mnist_test_cnn.rs
+++ src/mnist_test_cnn.rs
@@ -1,13 +1,14 @@
-use chrono::{Local};
+use chrono::Local;
 use mnist::{MnistBuilder, NormalizedMnist};
 use ndarray::{Array2, Array4, Ix0};
-use std::{fs::File, io::Write, time};
+use std::{f32::consts::PI, fs::File, io::Write, time};
 
 use crate::{
     nn_modules::{
         NNDataFlowTree, NNForwardInput, NNModule, NNModuleBuilder, ReshapeTensor, SoftmaxAndCELoss,
         builders::{
-            BatchNorm2dBuilder, ConvolutionBuilder, LinearBuilder, MaxPoolingBuilder, ReLUBuilder, ReshapeTensorBuilder, SoftmaxAndCELossBuilder
+            BatchNorm2dBuilder, ConvolutionBuilder, LinearBuilder, MaxPoolingBuilder, ReLUBuilder,
+            ReshapeTensorBuilder, SoftmaxAndCELossBuilder,
         },
     },
     rand::Rand,
@@ -56,6 +57,7 @@ pub fn mnist_process() -> Result<(), Box<dyn std::error::Error>> {
         .filter_size((3, 3))
         .filter_value(2)
         .padding(1)
+        .is_bias(false)
         .build();
     let batch1 = BatchNorm2dBuilder::new().channel_size(2).build();
     let relu_c1 = ReLUBuilder::new().build();
@@ -69,6 +71,7 @@ pub fn mnist_process() -> Result<(), Box<dyn std::error::Error>> {
         .input_image_size((14, 14))
         .filter_size((3, 3))
         .padding(1)
+        .is_bias(false)
         .build();
     let batch2 = BatchNorm2dBuilder::new().channel_size(4).build();
     let relu_c2 = ReLUBuilder::new().build();
@@ -81,6 +84,7 @@ pub fn mnist_process() -> Result<(), Box<dyn std::error::Error>> {
         .input_image_size((7, 7))
         .filter_size((3, 3))
         .filter_value(8)
+        .is_bias(false)
         .build();
     let batch3 = BatchNorm2dBuilder::new().channel_size(8).build();
     let relu_c3 = ReLUBuilder::new().build();
@@ -107,11 +111,11 @@ pub fn mnist_process() -> Result<(), Box<dyn std::error::Error>> {
     let pool_info1 = &tree.add(&relu_c_info1, pool1);
     let conv_info2 = &tree.add(&pool_info1, conv2);
     let batch_info2 = &tree.add(&conv_info2, batch2);
-    let relu_c_info2= &tree.add(&batch_info2, relu_c2);
+    let relu_c_info2 = &tree.add(&batch_info2, relu_c2);
     let pool_info2 = &tree.add(&relu_c_info2, pool2);
     let conv_info3 = &tree.add(&pool_info2, conv3);
     let batch_info3 = &tree.add(&conv_info3, batch3);
-    let relu_c_info3= &tree.add(&batch_info3, relu_c3);
+    let relu_c_info3 = &tree.add(&batch_info3, relu_c3);
 
     let reshape_info = &tree.add(&relu_c_info3, reshape);
 
@@ -120,7 +124,9 @@ pub fn mnist_process() -> Result<(), Box<dyn std::error::Error>> {
     let linear_info2 = &tree.add(&relu_info1, linear2);
     let output_info = &tree.add(&linear_info2, softmax_and_celoss);
 
-    let mut writing_file_string: String = "epoch\tlearning rate\tlearning error\tvalidation error\tvalidation collect rate\n".to_string();
+    let mut writing_file_string: String =
+        "epoch\tlearning rate\tlearning error\tvalidation error\tvalidation collect rate\n"
+            .to_string();
 
     // 処理時間計測用
     let epochs_now = time::Instant::now();
@@ -134,7 +140,13 @@ pub fn mnist_process() -> Result<(), Box<dyn std::error::Error>> {
 
         let mut mini_batch_count = 0;
         // let epoch_learning_rate = max_learning_rate + ((((epoch - 1) % 10) as ElementType / 9.0) * (min_learning_rate - max_learning_rate));
-        let epoch_learning_rate = max_learning_rate * ElementType::powf(0.02, ((epoch - 1) % 10) as ElementType / 9.0);
+        let epoch_learning_rate = if epoch <= 5 {
+            min_learning_rate + ((max_learning_rate - min_learning_rate) * (epoch - 1) as ElementType / 5.0)
+        } else {
+            min_learning_rate
+                + ((max_learning_rate - min_learning_rate) * ((((epoch - 6) as ElementType / 44.0) * PI).cos() + 1.0)
+                    / 2.0)
+        };
 
         println!("learning rate: {}", epoch_learning_rate);
 
@@ -156,7 +168,10 @@ pub fn mnist_process() -> Result<(), Box<dyn std::error::Error>> {
                 .into_scalar();
             _ = &tree.propagate_grad(None, epoch_learning_rate);
 
-            print!("\rmini batch count: {} / {}", mini_batch_count, mini_batches_per_epoch);
+            print!(
+                "\rmini batch count: {} / {}",
+                mini_batch_count, mini_batches_per_epoch
+            );
             std::io::stdout().flush().unwrap();
         }
 
@@ -166,7 +181,12 @@ pub fn mnist_process() -> Result<(), Box<dyn std::error::Error>> {
             epoch_error / training_value as ElementType
         );
 
-        writing_file_string += &format!("{}\t{}\t{}\t", epoch, epoch_learning_rate,  epoch_error / training_value as ElementType);
+        writing_file_string += &format!(
+            "{}\t{}\t{}\t",
+            epoch,
+            epoch_learning_rate,
+            epoch_error / training_value as ElementType
+        );
 
         // 検証部分
         println!("validation test:");
@@ -231,7 +251,11 @@ pub fn mnist_process() -> Result<(), Box<dyn std::error::Error>> {
         println!("              ({} / {})", result, validation_value);
         println!("error: {}\n", epoch_error / validation_value as ElementType);
 
-        writing_file_string += &format!("{}\t{}\n", epoch_error / validation_value as ElementType, result as ElementType / validation_value as ElementType);
+        writing_file_string += &format!(
+            "{}\t{}\n",
+            epoch_error / validation_value as ElementType,
+            result as ElementType / validation_value as ElementType
+        );
     }
 
     println!("after leaning test:");
@@ -290,21 +314,22 @@ pub fn mnist_process() -> Result<(), Box<dyn std::error::Error>> {
     println!("error: {}\n", test_error / test_value as ElementType);
 
     writing_file_string += "\nafter learning test\n";
-    writing_file_string += &format!("\tcollect rate\t{}\n", result as ElementType / test_value as ElementType);
+    writing_file_string += &format!(
+        "\tcollect rate\t{}\n",
+        result as ElementType / test_value as ElementType
+    );
     writing_file_string += &format!("\terror\t{}\n", test_error / test_value as ElementType);
 
     let epochs_process_duration = epochs_now.elapsed().as_secs_f64();
 
     // 処理時間表示
-    println!(
-        "epochs process duration: {}sec.",
-        epochs_process_duration
-    );
+    println!("epochs process duration: {}sec.", epochs_process_duration);
 
     writing_file_string += &format!("\nepochs process duration\t{}sec.", epochs_process_duration);
 
     // ファイル出力
-    let mut file = File::create("src/learning_results/".to_string() + &Local::now().to_string() + &".txt")?;
+    let mut file =
+        File::create("src/learning_results/".to_string() + &Local::now().to_string() + &".txt")?;
     // let buf = BufReader::new(io::stdin()).lines().collect::<io::Result<Vec<String>>>()?.join("\n");
     write!(file, "{}", writing_file_string)?;
     file.flush()?;
